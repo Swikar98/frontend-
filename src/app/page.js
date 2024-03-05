457b121d89e1ab2bdfe917a705c89d897c80c35c
Swikar98 <swikarsingh123@gmail.com>
2024-03-05 07:59:35 +0545

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-gray-300 text-blue-600 font-bold text-center p-4 m-4'> I am a frontend developer </div>
+  )
+}
+
+export default page
